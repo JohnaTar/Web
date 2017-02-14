1394dfd2f1739f5ffa97c3f880b2491365995c0e
johnatar <leavemetar@gmail.com>
2017-02-14 13:13:53 +0700

    fromcentos

---
+++ test_login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<head>
+<title>Authentication</title>
+<meta http-equiv="REFRESH" content="0;url=https://login.rmutsb.ac.th/login.php"></HEAD>
+<BODY>
+</BODY>
+</HTML>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<head>
+<title>Authentication</title>
+<meta http-equiv="REFRESH" content="0;url=https://login.rmutsb.ac.th/login.php"></HEAD>
+<BODY>
+</BODY>
+</HTML>
