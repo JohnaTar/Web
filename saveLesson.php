654cddd22aca3334ba83cd62843fc550f5efd5f7
Johnatar <leavemetar@gmail.com>
2017-03-09 15:58:22 +0700

    lllll

--- saveLesson.php
+++ saveLesson.php
@@ -4,7 +4,7 @@ if (isset($_POST['conntent'])) {
   $data = $_POST['conntent'];
   $less_id = $_POST['lesson'];
 
-  $sql = "INSERT INTO `stwContent` (`stwContent_id`, `stwLesson_id`, `stwContent_detail`) VALUES ('', '$less_id', '$data')";
+  $sql = "UPDATE `stwContent` SET `stwContent_detail`='$data' WHERE (`stwLesson_id`='$less_id') ";
   $result = mysqli_query($conn, $sql);
   if ($result) {
     echo "<script>alert('บันทึกข้อมูลเรียบร้อย')</script>";
