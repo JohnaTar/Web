fba89093bac40ed11eb68064ab244cb14f4f4584
Johnatar <leavemetar@gmail.com>
2017-03-16 09:26:43 +0700

    delete.exam

--- ckAjaxs-nattee.php
+++ ckAjaxs-nattee.php
@@ -2,7 +2,19 @@
 include("connect.php");
 
 if (isset($_POST['exam_id'])) {
-	 $sql ="DELETE FROM stwExam,stwExam_User,stwExam_detail";
+	
+	$sql ="DELETE stwExam,stwExam_User,stwExam_detail 
+		   FROM stwExam   
+		   INNER JOIN stwExam_User ON stwExam_User.stwExam_id = stwExam_User.stwExam_id 
+		   INNER JOIN stwExam_detail ON stwExam_detail.stwExam_id=stwExam_detail.stwExam_id 
+		   WHERE stwExam.stwExam_id ='".$_POST['exam_id']."'";
+	$reslut =mysqli_query($conn,$sql);
+	if ($reslut) {
+		 echo "ลบข้อมูลเรียบร้อย";
+	}else{
+		echo "ไม่สามารถลบข้อมูลได้";
+	}
+
 }
 
 
